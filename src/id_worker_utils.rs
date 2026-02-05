be57ec92b29191762b1bd77f28748650f733e457
zbz <nnzbz@qq.com>
2026-02-05 17:41:32 +0800

    * 优化代码

--- src/id_worker_utils.rs
+++ src/id_worker_utils.rs
@@ -1,12 +1,12 @@
 use crate::{IdWorker, IdWorkerConfig, IdWorkerGenerator, Options};
-use log::info;
+use log::debug;
 use std::sync::OnceLock;
 
 pub static ID_WORKER: OnceLock<Box<dyn IdWorker>> = OnceLock::new();
 
 /// 初始化id生成器
 pub fn init_id_worker(id_worker_config: IdWorkerConfig) {
-    info!("init id worker...");
+    debug!("init id worker...");
     let id_worker = IdWorkerGenerator::generate(
         Options::new()
             .epoch(id_worker_config.epoch)
