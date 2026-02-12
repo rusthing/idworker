eb708327ea45ae4fc95ba669b061061f78665c5d
zbz <nnzbz@qq.com>
2026-02-12 11:52:12 +0800

    * 优化代码

--- src/id_worker_utils.rs
+++ src/id_worker_utils.rs
@@ -4,21 +4,6 @@ use std::sync::{Arc, RwLock};
 
 static ID_WORKER: RwLock<Option<Arc<dyn IdWorker>>> = RwLock::new(None);
 
-/// 初始化id生成器
-pub fn init_id_worker(id_worker_config: IdWorkerConfig) -> Result<(), IdWorkerError> {
-    debug!("init id worker...");
-    let id_worker = IdWorkerGenerator::generate(
-        IdWorkerOptions::new()
-            .epoch(id_worker_config.epoch)?
-            .data_center(
-                id_worker_config.data_center,
-                id_worker_config.data_center_bits,
-            )?
-            .node(id_worker_config.node, id_worker_config.node_bits)?,
-    )?;
-    set_id_worker(id_worker)
-}
-
 /// 获取当前配置的只读访问
 pub fn get_id_worker() -> Result<Arc<dyn IdWorker>, IdWorkerError> {
     let read_lock = ID_WORKER.read().map_err(|_| IdWorkerError::GetIdWorker())?;
@@ -35,4 +20,19 @@ pub fn set_id_worker(value: Arc<dyn IdWorker>) -> Result<(), IdWorkerError> {
         .map_err(|_| IdWorkerError::SetIdWorker())?;
     *write_lock = Some(value);
     Ok(())
+}
+
+/// 初始化id生成器
+pub fn init_id_worker(id_worker_config: IdWorkerConfig) -> Result<(), IdWorkerError> {
+    debug!("init id worker...");
+    let id_worker = IdWorkerGenerator::generate(
+        IdWorkerOptions::new()
+            .epoch(id_worker_config.epoch)?
+            .data_center(
+                id_worker_config.data_center,
+                id_worker_config.data_center_bits,
+            )?
+            .node(id_worker_config.node, id_worker_config.node_bits)?,
+    )?;
+    set_id_worker(id_worker)
 }
